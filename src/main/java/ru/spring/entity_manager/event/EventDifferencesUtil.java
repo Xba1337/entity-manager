d4cf5a5684b83e298f151bf05d23467610814649
Kirill <chanchinkv@gmail.com>
2026-03-05 18:27:23 +0300

    Типовую операцию вынес в отдельный метод.

--- src/main/java/ru/spring/entity_manager/event/EventDifferencesUtil.java
+++ src/main/java/ru/spring/entity_manager/event/EventDifferencesUtil.java
@@ -15,69 +15,25 @@ public class EventDifferencesUtil {
 
     public Map<String, FieldChange<?>> changeMap(Event oldEvent, Event updatedEvent) {
         Map<String, FieldChange<?>> changes = new HashMap<>();
-        if (!Objects.equals(oldEvent.date(), updatedEvent.date())) {
-            changes.put(
-                    "date",
-                    new FieldChange<>(
-                            oldEvent.date(),
-                            updatedEvent.date()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.locationId(), updatedEvent.locationId())) {
-            changes.put(
-                    "locationId",
-                    new FieldChange<>(
-                            oldEvent.locationId(),
-                            updatedEvent.locationId()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.capacity(), updatedEvent.capacity())) {
-            changes.put(
-                    "capacity",
-                    new FieldChange<>(
-                            oldEvent.capacity(),
-                            updatedEvent.capacity()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.price(), updatedEvent.price())) {
-            changes.put(
-                    "price",
-                    new FieldChange<>(
-                            oldEvent.price(),
-                            updatedEvent.price()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.duration(), updatedEvent.duration())) {
-            changes.put(
-                    "duration",
-                    new FieldChange<>(
-                            oldEvent.duration(),
-                            updatedEvent.duration()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.status(), updatedEvent.status())) {
-            changes.put(
-                    "status",
-                    new FieldChange<>(
-                            oldEvent.status(),
-                            updatedEvent.status()
-                    )
-            );
-        }
-        if (!Objects.equals(oldEvent.name(), updatedEvent.name())) {
-            changes.put(
-                    "name",
-                    new FieldChange<>(
-                            oldEvent.name(),
-                            updatedEvent.name()
-                    )
-            );
-        }
+
+        putIfChanged(changes, "date", oldEvent.date(), updatedEvent.date());
+        putIfChanged(changes, "locationId", oldEvent.locationId(), updatedEvent.locationId());
+        putIfChanged(changes, "capacity", oldEvent.capacity(), updatedEvent.capacity());
+        putIfChanged(changes, "price", oldEvent.price(), updatedEvent.price());
+        putIfChanged(changes, "duration", oldEvent.duration(), updatedEvent.duration());
+        putIfChanged(changes, "status", oldEvent.status(), updatedEvent.status());
+        putIfChanged(changes, "name", oldEvent.name(), updatedEvent.name());
+
         return changes;
+    }
+
+    private <T> void putIfChanged(
+            Map<String, FieldChange<?>> changes,
+            String fieldName,
+            T oldValue,
+            T newValue) {
+        if (!Objects.equals(oldValue, newValue)) {
+            changes.put(fieldName, new FieldChange<>(oldValue, newValue));
+        }
     }
 }
